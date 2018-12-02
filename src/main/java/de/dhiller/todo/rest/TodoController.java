784e9fa26fe32730be3cc03366cbfeac9373afa0
Daniel Hiller <daniel.hiller.1972@gmail.com>
2018-12-02 18:49:16 +0100

    Add logging of remote call errors
    Add error handler and log exception in any case.

--- src/main/java/de/dhiller/todo/rest/TodoController.java
+++ src/main/java/de/dhiller/todo/rest/TodoController.java
@@ -1,6 +1,7 @@
 package de.dhiller.todo.rest;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -19,11 +20,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.ResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.*;
 
 import static java.util.Optional.ofNullable;
 
@@ -50,12 +50,35 @@ public class TodoController {
     @Autowired
     private UpdateReceiverRepository updateReceiverRepository;
 
-    @Autowired
-    private RestTemplateBuilder restTemplateBuilder;
-//            = new RestTemplateBuilder()
-//            .setConnectTimeout(3000)
-//            .errorHandler(new DefaultResponseErrorHandler())
-//            .build();
+    private RestTemplate restTemplate
+            = new RestTemplateBuilder()
+            .setConnectTimeout(3000)
+            .errorHandler(new ResponseErrorHandler() {
+                @Override
+                public boolean hasError(ClientHttpResponse response) throws IOException {
+                    return !response.getStatusCode().is2xxSuccessful();
+                }
+
+                @Override
+                public void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
+                    log.info("failed to notify client {}, response is {}", url, response);
+                    handleError(response);
+                }
+
+                @Override
+                public void handleError(ClientHttpResponse response) throws IOException {
+                    if (response.getStatusCode().is4xxClientError()) {
+                        log.error("failed to notify client, response is {}", response);
+                        throw new HttpClientErrorException(response.getStatusCode());
+                    } else if (response.getStatusCode().is5xxServerError()) {
+                        log.warn("server error while trying to notify remote, response is {}", response);
+                        throw new HttpServerErrorException(response.getStatusCode());
+                    } else {
+                        log.warn("server error while trying to notify remote, response is {}", response);
+                    }
+                }
+            })
+            .build();
 
     @GetMapping("/todos")
     public List<TodoDTO> listTodos(@RequestParam(value = "auth", required = false) String token,
@@ -72,11 +95,11 @@ public class TodoController {
 
     @PostMapping("/todos/{id}")
     public void updateTodo(@RequestParam(value = "auth", required = false) String token,
-                           @PathVariable(value="id") long id,
+                           @PathVariable(value = "id") long id,
                            @RequestBody(required = false) TodoDTO update) {
         final User authorizedUser = authenticationProvider.authorize(token);
         Todo todoToUpdate = getTodoByIdOrThrowNotFound(id);
-        if(!authorizedUser.equals(todoToUpdate.getUser()))
+        if (!authorizedUser.equals(todoToUpdate.getUser()))
             throw new TodoNotFoundException();
         todoToUpdate.setContent(update.getContent());
         todoToUpdate.setDone(update.isDone());
@@ -91,16 +114,23 @@ public class TodoController {
                 .forEach(executor::submit);
     }
 
-    private Runnable callEndpoint(final UpdateReceiver e, final TodoDTO afterUpdate) {
-        return () -> restTemplateBuilder.build().put(e.getEndpoint(), afterUpdate);
+    private Runnable callEndpoint(final UpdateReceiver updateReceiver, final TodoDTO afterUpdate) {
+        return () -> {
+            try {
+                restTemplate.put(updateReceiver.getEndpoint(), afterUpdate);
+            } catch (Exception ex) {
+                log.error("error while trying to notify remote", ex);
+                throw new RuntimeException(ex);
+            }
+        };
     }
 
     @GetMapping("/todos/{id}")
     public TodoDTO getTodo(@RequestParam(value = "auth", required = false) String token,
-                           @PathVariable(value="id") long id) {
+                           @PathVariable(value = "id") long id) {
         User authorizedUser = authenticationProvider.authorize(token);
         Todo todoToUpdate = getTodoByIdOrThrowNotFound(id);
-        if(!authorizedUser.equals(todoToUpdate.getUser()))
+        if (!authorizedUser.equals(todoToUpdate.getUser()))
             throw new TodoNotFoundException();
         return modelMapper.map(todoToUpdate, TodoDTO.class);
     }
